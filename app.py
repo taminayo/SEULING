07d1c7a26f3b1ab4de7dc673914b51fa5325924b
TAMI <tami@MC-TAEHYEON.local>
2019-05-21 14:05:51 +0900

    RENEW

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from target import Target
 from result import Result
-from writer import Writer
+import requests
+import csv
 
-target = Target()
-result = Result(target.nums)
-writer = Writer(target.column, target.nums, result.statusList)
+with open('raw.csv', 'r') as raw:
+	csv_reader = csv.DictReader(raw)
+	raw_fieldname = csv_reader.fieldnames[0]
+	with open('edited.csv', 'w') as edited:
+		fieldnames = [csv_reader.fieldnames[0], '상태']
+		
+		csv_writer = csv.DictWriter(edited, fieldnames=fieldnames)
+		csv_writer.writeheader()
+		for line in csv_reader:
+			num, status = Result(line[raw_fieldname]).getStatus()
+			csv_writer.writerow({fieldnames[0]:num, fieldnames[1]:status})
--- result.py
+++ result.py
@@ -1,22 +1,28 @@
+
 import requests
 
-class Result(object):
-    def __init__(self, nums):
-        self.nums = nums
-        self.statusList = []
-        self.pre = """<?xml version='1.0' encoding='utf-8'?>
-        <map id='ATTABZAA001R08'><pubcUserNo/><mobYn>N</mobYn>
-        <inqrTrgtClCd>1</inqrTrgtClCd>
-        <txprDscmNo>"""
-        self.post = """</txprDscmNo>
-        <dongCode>05</dongCode>
-        <psbSearch>Y</psbSearch>
-        <map id='userReqInfoVO'/>
-        </map>"""
-        self.requestsUrl = 'https://teht.hometax.go.kr/wqAction.do?actionId=ATTABZAA001R08&screenId=UTEABAAA13&popupYn=false&realScreenId='
+class Result:
+    
+    head = '''<?xml version='1.0' encoding='utf-8'?>
+        <map id='ATTABZAA001R08'><pubcUserNo/><mobYn>N</mobYn><inqrTrgtClCd>1</inqrTrgtClCd><txprDscmNo>'''
+    tail = '''</txprDscmNo><dongCode>05</dongCode><psbSearch>Y</psbSearch>
+        <map id='userReqInfoVO'/></map>'''
+    url = 'https://teht.hometax.go.kr/wqAction.do?actionId=ATTABZAA001R08&screenId=UTEABAAA13&popupYn=false&realScreenId='
+    headers = {'Content-Type': 'application/xml; charset=utf-8'}
 
-        for num in nums:
-            self.xml = self.pre+str(num)+self.post
-            self.headers = {'Content-Type': 'application/xml; charset=utf-8'}
-            self.status = requests.post(self.requestsUrl, data=self.xml, headers=self.headers).text.split('trtCntn')[1][1:-2]
-            self.statusList.append(self.status)
+    def __init__(self, num):
+        self.num = num
+        self.xml = '{}{}{}'.format(Result.head, num, Result.tail)
+        self.status = ''
+        
+    def getStatus(self):
+
+        if not self.num.isdigit() or not len(self.num) == 10:
+            self.status = '잘못된 형식입니다.'
+            return (self.num, self.status)
+        try:
+            self.status = requests.get(Result.url, data=self.xml, headers=Result.headers).text.split('trtCntn')[1][1:-2]
+        except Exception as e:
+            self.status = '{}'.format(e)
+        finally:
+            return (self.num, self.status)
