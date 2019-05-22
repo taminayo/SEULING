bd41dbd65f4c1f70c575c76b0c0ba1992cdec7cf
TAMI <tami@MC-TAEHYEON.local>
2019-05-22 18:37:08 +0900

    encoding settings & lineterminator

--- app/app.py
+++ app/app.py
@@ -6,13 +6,13 @@ import requests
 import csv
 print(requests)
 
-with open('raw.csv', 'r') as raw:
+with open('raw.csv', 'r', encoding='UTF-8') as raw:
 	csv_reader = csv.DictReader(raw)
 	raw_fieldname = csv_reader.fieldnames[0]
-	with open('edited.csv', 'w') as edited:
+	with open('edited.csv', 'w', encoding='UTF-8') as edited:
 		fieldnames = [csv_reader.fieldnames[0], '상태']
 		
-		csv_writer = csv.DictWriter(edited, fieldnames=fieldnames)
+		csv_writer = csv.DictWriter(edited, fieldnames=fieldnames, lineterminator='\n')
 		csv_writer.writeheader()
 		for line in csv_reader:
 			num, status = Result(line[raw_fieldname]).getStatus()
